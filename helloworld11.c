c765f8b00f2f019096498e2be7948d316c1267b3
Austin70 <47290149+Austin70@users.noreply.github.com>
2021-04-26 06:17:05 -0400

    Update helloworld11.c

--- helloworld11.c
+++ helloworld11.c
@@ -5,9 +5,15 @@
 
 int main(int argc,char *argv[])
 {	
-	int fact=1,i=2,n;
-	n=*argv[1];
-	n-=48;
+	int fact=1,i=0,n=0,temp;
+
+  while((temp=*(argv[1]+i))!=0)//ascii value of null character is zero
+{	temp-=48;
+	n=n*10+temp;//making it into decimal number from each character
+	i++;
+}
+
+i=2;	
 	while((i-2)<n)
 	{
 	
@@ -25,4 +31,3 @@ int main(int argc,char *argv[])
 	printf("\b\b");
 	return 0;
 }
-
